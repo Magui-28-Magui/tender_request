fc83d67fb9ff24d40f6c1156e0a1e1912c82ee4b
Magui Campos <88505411+MaguiCampos@users.noreply.github.com>
2022-02-21 14:37:31 -0800

    add cards in dashboard

--- src/index.php
+++ src/index.php
@@ -2,6 +2,6 @@
 include_once("includes/header.php");
 include_once("includes/topbar.php");
 include_once("includes/sidebar.php");
-include_once("views/home.php");
+include_once("views/dashboard/dashboard.php");
 include_once("includes/footer.php");
 ?>
