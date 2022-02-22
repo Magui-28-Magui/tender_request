f2ddd8073ae0463742ab065c1da2dea78db581e5
Magui Campos <88505411+MaguiCampos@users.noreply.github.com>
2022-02-22 10:47:14 -0800

    wizard form

--- src/views/tender_request/wizard/tender_request_form.php
+++ src/views/tender_request/wizard/tender_request_form.php
@@ -5,26 +5,26 @@
             <form action="">
                 <ul class="nav nav-pills mb-3 nav-justified" id="pills-tab" role="tablist">
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Step 1</button>
+                        <button class="nav-link active" id="pills-tender-tab" data-bs-toggle="pill" data-bs-target="#pills-tender" type="button" role="tab" aria-controls="pills-tender" aria-selected="true">Step 1</button>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Step 2</button>
+                        <button class="nav-link" id="pills-item-tab" data-bs-toggle="pill" data-bs-target="#pills-item" type="button" role="tab" aria-controls="pills-item" aria-selected="false">Step 2</button>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Step 3</button>
+                        <button class="nav-link" id="pills-delivery-tab" data-bs-toggle="pill" data-bs-target="#pills-delivery" type="button" role="tab" aria-controls="pills-delivery" aria-selected="false">Step 3</button>
                     </li>
                 </ul>
                 <div class="tab-content" id="pills-tabContent">
                     <!-- TENDER FORM -->
-                    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                    <div class="tab-pane fade show active" id="pills-tender" role="tabpanel" aria-labelledby="pills-tender-tab">
                         <?php require_once('tender_form.php'); ?>
                     </div>
                     <!-- ITEM FORM -->
-                    <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                    <div class="tab-pane fade" id="pills-item" role="tabpanel" aria-labelledby="pills-item-tab">
                         <?php require_once('item_form.php'); ?>
                     </div>
                     <!-- DELIVERY FORM -->
-                    <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+                    <div class="tab-pane fade" id="pills-delivery" role="tabpanel" aria-labelledby="pills-delivery-tab">
                         <?php require_once('delivery_form.php'); ?>
                     </div>
                 </div>
